87d540ba69d5ccd562e7458e7a7a2e1d257745f1
Rodrigo Oliveira <47666631+Rodrigo-Santoos@users.noreply.github.com>
2023-07-20 19:03:41 -0300

    Update 1.5
    * Colocando a camera para seguir o jogador

---
+++ Controle IDLE e Run/Assets/Scripts/ControllerCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerCamera : MonoBehaviour {
+
+	//camera
+	private Transform player;
+	private Vector2 velocidade;
+	public float smoothTimeX;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        player = GameObject.Find("Player").GetComponent<Transform>(); //pegando o Player e pegando o transform
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+	void FixedUpdate()
+    {
+		//aqui que a camera segue o jogador
+		float posX = Mathf.SmoothDamp(transform.position.x, player.position.x, ref velocidade.x, smoothTimeX);
+		transform.position = new Vector3(posX, transform.position.y, transform.position.z);
+    }
+}
